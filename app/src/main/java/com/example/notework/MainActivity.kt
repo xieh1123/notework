3757d6b58c5f60509b69d92512c65e9591cee733
xieh <chenrenhong168@gmail.com>
2025-04-15 21:35:07 +0800

    0415

--- app/src/main/java/com/example/notework/MainActivity.kt
+++ app/src/main/java/com/example/notework/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.notework
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,13 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    val textv1=findViewById<TextView>(R.id.textv1)
+    val but1=findViewById<Button>(R.id.button1)
+
+
+
+
+
+
     }
 }
